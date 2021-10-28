cb960d39f7fddc426df16d6438015dd028f67cf0
Torsten Giess <torsten.giess@ibtb.uni-stuttgart.de>
2021-10-28 17:38:30 +0200

    Restructured repo.

---
+++ utils/main.py
@@ -0,0 +1,13 @@
+from lxml import etree
+from saxslicer import SAXSlicer
+
+def main():
+    """Test some stuff."""
+    slic = SAXSlicer()
+    data = slic.extract_data()
+    print(data)
+    metadata = slic.extract_metadata()
+    print((etree.tostring(metadata.getroot())).decode("utf-8"))
+
+if __name__ == "__main__":
+    main()
