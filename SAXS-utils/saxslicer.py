e23bd5d04cad84c71f3a1a4f1c0b243c20153ba7
Torsten Giess <torsten.giess@ibtb.uni-stuttgart.de>
2021-10-28 16:00:44 +0200

    Refactored to PEP 8.

--- SAXS-utils/saxslicer.py
+++ SAXS-utils/saxslicer.py
@@ -2,30 +2,32 @@ import re
 import pandas as pd
 from lxml import etree
 
+
 class SAXSlicer:
     """Separate data block and XML metadata footer of a .pdh SAXS output file."""
-    
-    def __init__(self, inputFile: str = None):
-        if inputFile is None: inputFile = "Jupyter/datasets/SI06_210623[7].pdh"
-        self.inputFile = inputFile
 
-    def _line_is_xml(self, lineInFile):
+    def __init__(self, input_file: str = None):
+        if input_file is None: input_file = "Jupyter/datasets/SI06_210623[7].pdh"
+        self.input_file = input_file
+
+    def _line_is_xml(self, line_in_file):
         """Match n whitespaces, followed by an XML opening tag `<`."""
-        anyWhitespace = re.compile("\s*<").match(lineInFile)
-        return anyWhitespace
+        any_whitespace = re.compile("\s*<").match(line_in_file)
+        return any_whitespace
+    
     
     def extract_data(self) -> pd.DataFrame:
         """Extract data block as `pandas.DataFrame`."""
-        dataFrame = pd.read_table(self.inputFile, delimiter = "   ", usecols=[0,1], names = ["scatteringVector", "countsPerArea"], header=5, skipfooter=496, engine = "python")
-        return dataFrame
+        dataframe = pd.read_table(self.input_file, delimiter = "   ", usecols=[0,1], names = ["scattering_vector", "counts_per_area"], header=5, skipfooter=496, engine = "python")
+        return dataframe
 
-    def extract_metadata(self, directoryForXMLFile: str = None) -> etree.ElementTree:
+    def extract_metadata(self, directory_for_XML_file: str = None) -> etree.ElementTree:
         """Extract XML metadata footer as `etree.ElementTree` and save to file."""
-        if directoryForXMLFile is None: directoryForXMLFile = "Jupyter/datasets/xml-test.xml"
-        with open(self.inputFile, "r") as rf:
-            with open(directoryForXMLFile, "w") as wf:
+        if directory_for_XML_file is None: directory_for_XML_file = "Jupyter/datasets/xml-test.xml"
+        with open(self.input_file, "r") as rf:
+            with open(directory_for_XML_file, "w") as wf:
                 for line in rf:
                     if self._line_is_xml(line) is not None:
                         wf.write(line)
-        XMLTree = etree.parse(directoryForXMLFile)
-        return XMLTree
+        XML_tree = etree.parse(directory_for_XML_file)
+        return XML_tree
