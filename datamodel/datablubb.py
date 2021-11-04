a84cba0c7d046daa7150fcf3b7ace84638f113d9
JR-1991 <30547301+JR-1991@users.noreply.github.com>
2021-11-04 12:07:41 +0100

    Added example and updated utilities

--- datamodel/datablubb.py
+++ datamodel/datablubb.py
@@ -1,5 +1,6 @@
 """ Datamodel for SAXS with export functionalities to JSON and XML. """
 
+
 __all__ = []
 __version__ = "0.1.0"
 __author__ = "Torsten Giess"
@@ -7,101 +8,89 @@ __author__ = "Torsten Giess"
 from typing import List, Union, Optional
 
 # import pandas as pd
-from pydantic.dataclasses import dataclass
-
-from utilities import SchemaBase, attribute, element
-
-
-# @dataclass
-# class Data(SchemaBase):
-#     """ `pandas.DataFrame` of SAXS data. """
-
-#     data: pd.DataFrame
+from dataclasses import dataclass
+from datamodel.softdata import SchemaBase, attribute, element
 
 
 @dataclass
 class Value(SchemaBase):
     """ Values are the endpoints of the metadata tree. """
-    
-    key: Optional[str] = attribute(name="key")
-    name: Optional[str] = attribute(name="name")
-    value: Optional[str] = attribute(name="value")
-    stddev: Optional[str] = attribute(name="stddev")
-    unit: Optional[str] = attribute(name="unit")
-    quantity: Optional[str] = attribute(name="quantity")
-    counts: Optional[str] = attribute(name="counts")
-    type_: Optional[str] = attribute(name="type")
-    db: Optional[str] = attribute(name="db")
-    link: Optional[str] = attribute(name="link")
-    list_: Optional[str] = attribute(name="list")
-    meaning: Optional[str] = attribute(name="meaning")
-    index: Optional[int] = attribute(name="index")
-    positionindex: Optional[int] = attribute(name="positionindex")
-    waxsindex: Optional[int] = attribute(name="waxsindex")
-    x: Optional[int] = attribute(name="X")
-    y: Optional[int] = attribute(name="Y")
-    tol: Optional[Union[int, float]] = attribute(name="tol")
-    min_: Optional[Union[int, float]] = attribute(name="min")
-    max_: Optional[Union[int, float]] = attribute(name="max")
-    value: str
+
+    value: object
+    # key: Optional[str] = attribute(name="key")
+    # name: Optional[str] = attribute(name="name")
+    # value_att: Optional[str] = attribute(name="value")
+    # stddev: Optional[str] = attribute(name="stddev")
+    # unit: Optional[str] = attribute(name="unit")
+    # quantity: Optional[str] = attribute(name="quantity")
+    # counts: Optional[str] = attribute(name="counts")
+    # type_: Optional[str] = attribute(name="type")
+    # db: Optional[str] = attribute(name="db")
+    # link: Optional[str] = attribute(name="link")
+    # list_: Optional[str] = attribute(name="list")
+    # meaning: Optional[str] = attribute(name="meaning")
+    # index: Optional[int] = attribute(name="index")
+    # positionindex: Optional[int] = attribute(name="positionindex")
+    # waxsindex: Optional[int] = attribute(name="waxsindex")
+    # x: Optional[int] = attribute(name="X")
+    # y: Optional[int] = attribute(name="Y")
+    # tol: Optional[Union[int, float]] = attribute(name="tol")
+    # min_: Optional[Union[int, float]] = attribute(name="min")
+    # max_: Optional[Union[int, float]] = attribute(name="max")
 
 
 @dataclass
 class List_(SchemaBase):
     """ Lists contain instances of `Value`. """
-    
+
     key: str = attribute(name="key")
     islimited: Optional[bool] = attribute(name="islimited")
-    value: List[Value] = element(name="value")
+    value: List[Value] = element(name="value", default=list)
 
 
 @dataclass
 class Group(SchemaBase):
     """ Groups contain instances of `Value` and `List_`. """
-    
+
     isdevice: bool = attribute(name="isdevice")
     category: str = attribute(name="category")
     sub: str = attribute(name="sub")
     key: str = attribute(name="key")
-    id_: str = attribute(name="id")
-    list_: List[List_] = element(name="list")
-    value: List[Value] = element(name="value")
+    id: str = attribute(name="id")
+    list_: List[List_] = element(name="list", default=list)
+    value: List[Value] = element(name="value", default=list)
 
 
 @dataclass
 class Setup(SchemaBase):
     """ Setup contains instrument-related instances of `Group`. """
-    
-    group: List[Group] = element(name="group")
+
+    groups: List[Group] = element(name="group", default=list)
 
 
 @dataclass
 class Parameter(SchemaBase):
     """ Parameters describe the measurement program through instances of `Value`. """
-    
+
     key: str = attribute(name="key")
-    type_: Optional[str] = attribute(name="type")
+    type: Optional[str] = attribute(name="type")
     index: Optional[int] = attribute(name="idex")
-    value: List[Value] = element(name="value")
+    value: List[Value] = element(name="value", default=list)
 
 
 @dataclass
 class Column(SchemaBase):
-    """ Columns describe the data columns through instances of `Value`. """    
-    
+    """ Columns describe the data columns through instances of `Value`. """
+
     key: str = attribute(name="key")
-    value: List[Value] = element(name="value")
+    value: List[Value] = element(name="value", default=list)
 
 
 @dataclass
 class Fileinfo(SchemaBase):
     """ Metadata class for SAXS datamodel. """
-    
+
     version: str = attribute(name="version")
-    column: List[Column] = element(name="column")
-    parameter: List[Parameter] = element(name="parameter")
-    setup: List[Setup] = element(name="setup")
-
-
-if __name__ == "__main__":
-    pass
+    columns: List[Column] = element(name="column", default=list)
+    parameters: List[Parameter] = element(name="parameter", default=list)
+    setup: Setup = element(name="setup", default=Setup)
